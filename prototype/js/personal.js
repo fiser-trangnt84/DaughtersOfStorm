c86ae2feeb1c1d6b01aa332a3f62f7e2c7908f73
fiser-trangnt84 <trangnt84@fpt.com.vn>
2015-11-12 20:01:46 -0800

    update personal.html

---
+++ prototype/js/personal.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.editButton').click(function(e){
+		e.preventDefault();
+		$('.edit').html('<input type="text" placeholder="">');
+		for (var i = 0; i < $('.edit').length; i++) {
+			var str;
+		}
+	});
+});
