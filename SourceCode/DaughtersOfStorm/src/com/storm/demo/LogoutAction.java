3fc81397e2b40ab4e189fd30103f1c0290d80954
fiser-hiendt24 <helendth@gmail.com>
2015-11-21 22:07:59 +0700

    delete information from session after logout

--- SourceCode/DaughtersOfStorm/src/com/storm/demo/LogoutAction.java
+++ SourceCode/DaughtersOfStorm/src/com/storm/demo/LogoutAction.java
@@ -1,8 +1,13 @@
 package com.storm.demo;
 
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
 import com.opensymphony.xwork2.ActionSupport;
 
-public class LogoutAction extends ActionSupport {
+public class LogoutAction extends ActionSupport implements SessionAware {
+    private Map<String, Object> sessionMap;
 	private int userId;
 	private static final long serialVersionUID = 1L;
 
@@ -11,6 +16,8 @@ public class LogoutAction extends ActionSupport {
 		int rs = ConnectionDB.doLogout(this);
 		if (rs != 0){
 			ret = SUCCESS;
+			sessionMap.remove("username");
+			sessionMap.remove("userId");
 		}
 			
 		return ret;
@@ -22,5 +29,16 @@ public class LogoutAction extends ActionSupport {
 
 	public void setUserId(int userId) {
 		this.userId = userId;
+	}
+
+	@Override
+	public void setSession(Map<String, Object> sessionMap) {
+		// TODO Auto-generated method stub
+		this.sessionMap = sessionMap;
+		
+	}
+
+	public Map<String, Object> getSessionMap() {
+		return sessionMap;
 	}
 }
