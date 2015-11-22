fa5b96dc84ebe57ee6cc73988ad07c15f3643937
fiser-hiendt24 <helendth@gmail.com>
2015-11-23 00:04:44 +0700

    add to cart

---
+++ SourceCode/DaughtersOfStorm/src/com/storm/demo/AddFavoriteAction.java
@@ -0,0 +1,72 @@
+package com.storm.demo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class AddFavoriteAction extends ActionSupport implements SessionAware {
+    private Map<String, Object> sessionMap;
+	private static final long serialVersionUID = 1L;
+	private int productCode;
+    private int userId;
+
+    
+    public String execute(){
+        String ret = ERROR;
+        userId = (int) sessionMap.get("userId");
+        Connection conn = ConnectionDB.getConnection();
+        try{
+        	String sql = "SELECT * FROM favoritelists WHERE userId = " + userId;
+        	PreparedStatement ps = conn.prepareStatement(sql);
+        	ResultSet rs = ps.executeQuery();
+        	if (rs.next()){
+        		int pd = rs.getInt("productCode");
+        		if (pd == productCode) return ret;
+        		sql = "INSERT INTO favoritelists(favoriteNumber, productCode, userId)"
+        				+ " VALUES (?, ?, ?)";
+        		ps = conn.prepareStatement(sql);
+        		ps.setInt(1, rs.getInt("favoriteNumber"));
+        		ps.setInt(2, productCode);
+        		ps.setInt(3, userId);
+        		int result = ps.executeUpdate();   
+        		
+        		if (result != 0){
+        			ret = SUCCESS;
+        		}
+        	}
+        } catch (Exception e){
+        	e.printStackTrace();
+        }
+        return ret;
+    }
+ 
+	@Override
+	public void setSession(Map<String, Object> sessionMap) {
+		// TODO Auto-generated method stub
+		this.sessionMap = sessionMap;
+	}
+
+	public Map<String, Object> getSessionMap() {
+		return sessionMap;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getProductCode() {
+		return productCode;
+	}
+
+	public void setProductCode(int productCode) {
+		this.productCode = productCode;
+	}
+}
