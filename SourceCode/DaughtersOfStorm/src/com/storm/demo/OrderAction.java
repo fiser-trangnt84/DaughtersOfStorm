37d3c11b832816f9311e55817756d4204ac5fabe
fiser_TrangNT83 <nguyentrang231095@gmail.com>
2015-11-24 08:42:21 -0800

    Update OrderAction.java

--- SourceCode/DaughtersOfStorm/src/com/storm/demo/OrderAction.java
+++ SourceCode/DaughtersOfStorm/src/com/storm/demo/OrderAction.java
@@ -3,14 +3,17 @@ import java.sql.ResultSet;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
 //import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 
+import org.apache.struts2.interceptor.SessionAware;
+
 import com.opensymphony.xwork2.ActionSupport;
 @SuppressWarnings("serial")
-public class OrderAction extends ActionSupport {
+public class OrderAction extends ActionSupport implements SessionAware{
 
 	/**
 	 * 
@@ -29,6 +32,7 @@ public class OrderAction extends ActionSupport {
 	private String recipientPhoneNumber;
 	private String recipientAddress;
 	private String userId;
+	private Map<String, Object> sessionmap;
 	
 	
 	public String execute() {
@@ -73,15 +77,17 @@ public class OrderAction extends ActionSupport {
 		 orderDate = dateFormat.format(date);
 	     System.out.println(orderDate);
 	     int last_id = 0;
-	     
+	     int id = 0;
+	    
 	     try {
 	       
+	       id = (int) sessionmap.get("userId");
 	       String sql0 = "insert  into  orders (orderdate, status,comments,userId) values(?,?,?,?)";
 	       PreparedStatement ps0 = conn.prepareStatement(sql0);
 	       ps0.setString(1,orderDate);
 	       ps0.setString(2,"Inprogress");
 	       ps0.setString(3, "coming soon");
-	       ps0.setString(4, userId);
+	       ps0.setInt(4, id);
 	       ps0.executeUpdate();
 	     
 	       
@@ -266,6 +272,12 @@ public class OrderAction extends ActionSupport {
 
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
+	}
+
+	@Override
+	public void setSession(Map<String, Object> arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
