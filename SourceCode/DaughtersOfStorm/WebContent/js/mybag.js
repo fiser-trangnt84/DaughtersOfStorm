4d84e7b822534606e287112aa0d1e9c8318a7585
fiser-thomlt6 <ThomLT6@fpt.com.vn>
2015-11-25 12:17:19 -0800

    mybag.js

--- SourceCode/DaughtersOfStorm/WebContent/js/mybag.js
+++ SourceCode/DaughtersOfStorm/WebContent/js/mybag.js
@@ -3,21 +3,16 @@ $(document).ready(function(){
     $('h5.countItems').text("The number of items: " + numItems);
 
     $(".icon a").click(function(event){
-        if(confirmDelete()) {
-            $(this).parent().parent().fadeOut(400);;
-            numItems--;
-            $('h5.countItems').text("The number of items: " + numItems);
-        } 
+    
+        $(this).parent().parent().fadeOut(400);;
+        numItems--;
+        $('h5.countItems').text("The number of items: " + numItems);
+
     });
+    
     var username= $('#username').text();
 
     $('.user span #registerform').text("Log Out");
     $('.user span #loginform').text(username);
-    
-    function confirmDelete() {
-        var agree = confirm("Are you sure you want to delete this product?");
-        if(agree) return true;
-        else return false;
-    }
 
 });
