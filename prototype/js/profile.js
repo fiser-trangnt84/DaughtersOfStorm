2a5d61834faa490195e0a5c43702e6285b2c237e
fiser-hiendt24 <helendth@gmail.com>
2015-11-25 14:03:29 +0700

    delete useless folder

--- prototype/js/profile.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$('.editButton').click(function(e){
-		e.preventDefault();
-		$('.edit').html('<input type="text" placeholder="">');
-		for (var i = 0; i < $('.edit').length; i++) {
-			var str;
-		}
-	});
-});
